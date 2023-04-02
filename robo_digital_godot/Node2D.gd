36d43765f71e911598a90964e09626b400bf9f45
Luiz Fernando da Silva Borges <40524905+luizfsborges@users.noreply.github.com>
2023-04-02 16:57:58 -0300

    [Resolução de conflitos nas movimentações entre x e y e adição de de coordenadas na interface grafica]

--- robo_digital_godot/Node2D.gd
+++ robo_digital_godot/Node2D.gd
@@ -3,6 +3,15 @@ extends Node2D
 var client = WebSocketClient.new()
 var url = "ws://localhost:5000"
 
+var zero_x = 695
+var zero_z = 400
+
+var limite_x_direita = 950
+var limite_x_esquerda = 460
+
+var limite_z_cima = 130
+var limite_z_baixo = 400
+
 func _ready():
 	#client.connect("connection_closed", self, "connection_closed")
 	#client.connect("connection_error", self, "connection_error")
@@ -14,12 +23,17 @@ func _ready():
 	if erro!= OK:
 		set_process(false)
 		print("Conexão mal sucedida")
+		
+	get_node("Label_X").get_child(0).set_text(str(zero_x))
+	get_node("Label_Z").get_child(0).set_text(str(zero_z))
+	
+	$Sprite_garra.position = Vector2(zero_x, zero_z)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-
-	#mostra_coordenada_x(678)
+	
 	client.poll()
+
+	#print(get_viewport().get_mouse_position())
 	
 	if $Button_frente.pressed:
 		decrementar_y()
@@ -35,35 +49,41 @@ func _process(delta):
 		decrementar_z()
 	
 func exibe_feedback(mensagem_retorno):
-	get_node("Label_feedback_coordenadas").set_text(str(mensagem_retorno))
+	var mensagem_exibicao = str(mensagem_retorno.replace('"', '').replace(':', ': '))
+	get_node("Label_feedback_coordenadas").set_text(mensagem_exibicao)
 	
 func mensagem_recebida():
 	var mensagem = client.get_peer(1).get_packet().get_string_from_utf8()
 	exibe_feedback(mensagem)
 	
 	var movimentos = mensagem.replace('"', '').split(":")
+	
 	var movimento_x = float(movimentos[1])
 	var movimento_y = float(movimentos[3])
 	var movimento_z = float(movimentos[5])
-	
-	$Sprite_garra.move_local_x(movimento_x)
-	$Sprite_garra.move_local_y(movimento_y)
-	$Sprite_garra.scale.x = movimento_z*0.01
-	$Sprite_garra.scale.y = movimento_z*0.01
-	
+
+	var x_atual = $Sprite_garra.position.x
+	var z_atual = $Sprite_garra.position.y
+
+	mover_garra(x_atual, z_atual, movimento_x, movimento_z, $Sprite_garra)
 
 func manda_mensagem(mensagem_enviada):
 	client.get_peer(1).put_packet(JSON.print(mensagem_enviada).to_utf8())
 	
-
-var incremento_x = 0
+var incremento_x = zero_x
 func incrementar_x():
-	incremento_x += 1
-	get_node("Label_X").get_child(0).set_text(str(incremento_x))
+	if incremento_x <= limite_x_direita:
+		incremento_x += 1
+		get_node("Label_X").get_child(0).set_text(str(incremento_x))
+	else:
+		get_node("Label_X").get_child(0).set_text(str(limite_x_direita))
 	
 func decrementar_x():
-	incremento_x -= 1
-	get_node("Label_X").get_child(0).set_text(str(incremento_x))
+	if incremento_x >= limite_x_esquerda:
+		incremento_x -= 1
+		get_node("Label_X").get_child(0).set_text(str(incremento_x))
+	else:
+		get_node("Label_X").get_child(0).set_text(str(limite_x_esquerda))
 	
 var incremento_y = 0
 func incrementar_y():
@@ -74,15 +94,21 @@ func decrementar_y():
 	incremento_y -= 1
 	get_node("Label_Y").get_child(0).set_text(str(incremento_y))
 	
-var incremento_z = 0
+var incremento_z = zero_z
 func incrementar_z():
-	incremento_z += 1
-	get_node("Label_Z").get_child(0).set_text(str(incremento_z))
+	if incremento_z <= limite_z_baixo:
+		incremento_z += 1
+		get_node("Label_Z").get_child(0).set_text(str(incremento_z))
+	else:
+		get_node("Label_Z").get_child(0).set_text(str(limite_z_baixo))
 	
 func decrementar_z():
-	incremento_z -= 1
-	get_node("Label_Z").get_child(0).set_text(str(incremento_z))
-	
+	if incremento_z >= limite_z_cima:
+		incremento_z -= 1
+		get_node("Label_Z").get_child(0).set_text(str(incremento_z))
+	else:
+		get_node("Label_Z").get_child(0).set_text(str(limite_z_cima))
+		
 func _on_Button_enviar_pressed():
 	var coordenada_x = get_node("Label_X").get_child(0).text
 	var coordenada_y = get_node("Label_Y").get_child(0).text
@@ -90,3 +116,35 @@ func _on_Button_enviar_pressed():
 	var coordenadas_movimentacao = "X:" + coordenada_x + ":Y:" + \
 	coordenada_y + ":Z:" + coordenada_z
 	manda_mensagem(coordenadas_movimentacao)
+	
+func mover_garra(posicao_atual_x, posicao_atual_z, \
+				posicao_x, posicao_z, sprite_movimentacao):
+
+	var delta_deslocamento_x = abs(posicao_x - posicao_atual_x)
+	var delta_deslocamento_z = abs(posicao_z - posicao_atual_z)
+	var delta_deslocamento = max(delta_deslocamento_x, delta_deslocamento_z)
+	
+	if delta_deslocamento_x > 0:
+
+		for i in range(delta_deslocamento_x):
+			if i < delta_deslocamento_x:
+				if posicao_x > posicao_atual_x:
+					posicao_atual_x += 1
+					sprite_movimentacao.position = Vector2(posicao_atual_x, posicao_atual_z)
+					
+				elif posicao_x < posicao_atual_x:
+					posicao_atual_x-= 1
+					sprite_movimentacao.position = Vector2(posicao_atual_x, posicao_atual_z)
+			yield(get_tree().create_timer(0.01), "timeout")
+					
+	if delta_deslocamento_z > 0:
+			
+		for i in range(delta_deslocamento_z):
+			if posicao_z > posicao_atual_z:
+				posicao_atual_z += 1
+				sprite_movimentacao.position = Vector2(posicao_atual_x, posicao_atual_z)
+			
+			elif posicao_z < posicao_atual_z:
+				posicao_atual_z-=1
+				sprite_movimentacao.position = Vector2(posicao_atual_x, posicao_atual_z)
+			yield(get_tree().create_timer(0.01), "timeout")
